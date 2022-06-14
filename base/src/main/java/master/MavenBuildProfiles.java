392f4372e18dbf31f897ed7adcf5cc0f32820b8d
tchen <tchen@murex.com>
2022-06-14 14:36:35 +0200

    add maven files and profile demo

--- base/src/main/java/master/MavenBuildProfiles.java
+++
@@ -1,8 +0,0 @@
-package master;
-
-// 使用<profile>配置，在运行maven的时候传递不同的参数
-// https://maven.apache.org/guides/introduction/introduction-to-profiles.html
-// https://mkyong.com/maven/maven-profiles-example/
-public class MavenBuildProfiles {
-
-}
